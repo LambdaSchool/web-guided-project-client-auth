21c9284f5c85a8ffff1535ad6f6e4d74e0bc703a
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-11 17:59:13 -0800

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, useHistory } from 'react-router-dom';
 
 import Login from './components/Login';
 import GasPrices from './components/GasPrices';
-
-import axios from 'axios';
 
 function App() {
   let history = useHistory();
