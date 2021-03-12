e7b286fd27ced347aeb1fa4ff92e731f696d2adb
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-11 17:52:53 -0800

    lecture work-in-progress

--- client/src/components/GasPrices.js
+++ client/src/components/GasPrices.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import Loader from 'react-loader-spinner';
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 class GasPrices extends React.Component {
   state = {
@@ -14,7 +14,7 @@ class GasPrices extends React.Component {
 
   getData = () => {
     const token = localStorage.getItem("authToken");
-    axios.get("http://localhost:5000/api/data", { headers: { Authorization: token } })
+    axiosWithAuth().get("http://localhost:5000/api/data", { headers: { Authorization: token } })
       .then(res => {
         this.setState({
           ...this.state, // not necessary with setState's shallow merge, but we like to practice it this way whenever using objects as state variables, because that's how we do it in Redux!
